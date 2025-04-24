908d5357565df52e59219a03fdad10dc66e8efdd
CIRO BEN DOV <48111117@ort>
2025-04-24 12:14:55 -0300

    componente cita

---
+++ proyecto/src/components/tarjetaCita/tarjetaCita.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './tarjetaCita.css'
+
+const tarjetaCita = () => {
+    return(
+        <div className="cita">
+            <p>Mascota: <span>Nina</span></p>
+            <p>Dueño: <span>Martin</span></p>
+            <p>Fecha: <span>2021-08-05</span></p>
+            <p>Hora: <span>08:20</span></p>
+            <p>Sintomas: <span>Le duele la pierna</span></p><button class="button elimnar u-full-width">Eliminar ×</button>
+        </div>
+    )
+}
+
+export default tarjetaCita
